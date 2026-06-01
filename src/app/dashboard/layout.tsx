34cb22a66ab91cbad50978d0c1b0a35565918296
Amazingsoft <148471754+amazingsoft55@users.noreply.github.com>
2026-06-01 23:45:25 +0300

    kaydıramama sorunu çözüldü

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -130,14 +130,12 @@ export default function DashboardLayout({
       </header>
 
       {/* ══════ MAIN CONTENT ══════ */}
-      <main className="flex-1 overflow-y-auto overscroll-contain sm:pb-0 pb-20">
-        <div className="h-full">
-          <PushNotificationProvider>
-            <FeatureGateProvider>
-              {children}
-            </FeatureGateProvider>
-          </PushNotificationProvider>
-        </div>
+      <main className="flex-1 min-h-0 overflow-y-auto overscroll-contain sm:pb-0 pb-20">
+        <PushNotificationProvider>
+          <FeatureGateProvider>
+            {children}
+          </FeatureGateProvider>
+        </PushNotificationProvider>
       </main>
 
       {/* ══════ MOBILE BOTTOM NAV ══════ */}
